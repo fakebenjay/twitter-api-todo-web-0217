caf5cbb1acb90cc5969a2000571990eb996751e3
fakebenjay <fakebenjay@gmail.com[color]>
2017-02-08 15:56:33 -0500

    Done.

--- twitter.rb
+++ twitter.rb
@@ -1,5 +1,6 @@
 require 'twitter'
 require 'yaml'
+require 'pry'
 
 class TwitterApi
   attr_reader :client
@@ -16,30 +17,44 @@ class TwitterApi
 
 
   def most_recent_friend
-    #find the twitter gem method that retrieves a user's friends and grab the most recent friend
+    client.friends.first
   end
 
   def find_user_for(username)
-    #find the twitter gem method that returns the correct user, given a username
+    client.user(username)
   end
 
   def find_followers_for(username)
-    #find the twitter gem method that returns the follows of a given user
+    client.followers(username).take(10)
   end
 
   def homepage_timeline
-    #find the twitter gem method that retreives the tweets from a user's timeline.
+    client.home_timeline
   end
-  
+
 end
 
-#Bonus: 
+#Bonus:
 
 # uncomment out the following and read the bonus instructions.
 # remember to comment out the WebMock line of your spec_helper, as the instructions dictate.
 
-# tweet_client = TwitterApi.new
-# puts tweet_client.most_recent_friend
-# puts tweet_client.find_user_for("USERNAME HERE")
-# puts tweet_client.find_followers_for("USERNAME HERE")
-# puts tweet_client.homepage_timeline
+ tweet_client = TwitterApi.new
+ puts "You most recently followed:"
+ puts tweet_client.most_recent_friend.name
+ puts "*******"
+
+ puts "Your name is:"
+ puts tweet_client.find_user_for("FakeBenJay").name
+ puts "*******"
+
+ puts "Your ten newest followers are:"
+ tweet_client.find_followers_for("FakeBenJay").each do |follower|
+   puts follower.name
+ end
+ puts "*******"
+ puts "Your home timeline:"
+tweet_client.homepage_timeline.collect do |tweet|
+  puts "......."
+  puts "#{tweet.user.name} (@#{tweet.user.screen_name}): #{tweet.text}"
+end
